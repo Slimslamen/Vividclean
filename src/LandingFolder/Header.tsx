a30781c0b878ce5bd3122b7ad275064296e1820e
martamalm <144331091+martamalm@users.noreply.github.com>
2024-02-26 12:36:45 +0100

    lagt in heroimage och text

--- src/LandingFolder/Header.tsx
+++ src/LandingFolder/Header.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Checkbox, Label } from 'flowbite-react';
+
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="relative w-full h-screen">
+      <div className="relative">
+        <img
+          className="w-full h-auto object-cover opacity-60"
+          src="src/assets/heroimage3.jpg"
+          alt="heroimage"
+        />
+        <div className="absolute top-0 w-full h-full flex justify-end items-end p-12">
+          <div className="text-center mb-56">
+            <h1 className="text-5xl text-black mb-5">Välkommen till VividClean</h1>
+            <p className="text-lg mb-5">FÅ 15% RABATT PÅ DIN FÖRSTA BOKNING</p>
+            <div className="flex max-w-md flex-col gap-4" id="checkbox">
+      <div className="flex items-center gap-2">
+      <input type="checkbox" id="filled-checkbox" className="hidden" checked />
+  <label htmlFor="filled-checkbox" className="font-bold flex items-center cursor-not-allowed">
+    <span className="w-6 h-6 bg-green-500 rounded-md flex justify-center items-center mr-2">
+      <svg className="w-4 h-4 text-white fill-current" viewBox="0 0 20 20">
+        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+      </svg>
+    </span>
+    5/5 i betyg från våra kunder
+  </label>
+      </div>
+      <div className="flex items-center gap-2">
+      <input type="checkbox" id="filled-checkbox" className="hidden" checked />
+  <label htmlFor="filled-checkbox" className="font-bold flex items-center cursor-not-allowed">
+    <span className="w-6 h-6 bg-green-500 rounded-md flex justify-center items-center mr-2">
+      <svg className="w-4 h-4 text-white fill-current" viewBox="0 0 20 20">
+        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+      </svg>
+    </span>
+    Kollektivavtal
+  </label>
+      </div>
+      <div className="flex items-center gap-2">
+      <input type="checkbox" id="filled-checkbox" className="hidden" checked />
+  <label htmlFor="filled-checkbox" className="font-bold flex items-center cursor-not-allowed">
+    <span className="w-6 h-6 bg-green-500 rounded-md flex justify-center items-center mr-2">
+      <svg className="w-4 h-4 text-white fill-current" viewBox="0 0 20 20">
+        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+      </svg>
+    </span>
+    100% nöjd-kund-garanti
+  </label>
+      </div>
+      <div className="flex gap-2">
+        <div className="flex flex-col">
+        <input type="checkbox" id="filled-checkbox" className="hidden" checked />
+  <label htmlFor="filled-checkbox" className="flex items-center cursor-not-allowed font-bold">
+    <span className="w-6 h-6 bg-green-500 rounded-md flex justify-center items-center mr-2">
+      <svg className="w-4 h-4 text-white fill-current" viewBox="0 0 20 20">
+        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+      </svg>
+    </span>
+    Ingen bindningstid
+  </label>
+        </div>
+      </div>
+    </div>
+            <button className="bg-gray-700 rounded p-2 text-white flex items-center ml-36 mt-10">Ta del av erbjudandet nu</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
