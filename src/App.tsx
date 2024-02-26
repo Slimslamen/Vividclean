a679cf55ab01a12cc707c72e0a3c6b6da2e5e985
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-02-26 11:02:46 +0100

    Replace button with Landing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from 'flowbite-react';
+import Landing from "./LandingFolder/Landing";
+
 
 function App() {
   return (
     <>
-      <Button>Click me</Button>
+        <Landing />
     </>
   );
 }
---
+++ src/LandingFolder/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import Main from './Main'
+import Header from './Header'
+
+export default function Landing() {
+  return (
+    <>
+        <Header />
+        <Main />
+        <Footer/>
+    </>
+  )
+}
