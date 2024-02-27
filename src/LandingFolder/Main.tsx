4e8445e1acc26dfdb57e2ac2495202657b4344c4
martamalm <144331091+martamalm@users.noreply.github.com>
2024-02-27 10:27:33 +0100

    header heroimage

--- src/LandingFolder/Main.tsx
+++ src/LandingFolder/Main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Carousel from "./Carousel";
 
 export default function Main() {
-  return <div>Main</div>;
+  const images: string[] = ["src/assets/dammsugare.jpg.png",
+    "src/assets/heroimage2.jpg",
+    "src/assets/heroimage3.jpg",]
+  return <div>
+<Carousel images={images} />
+    
+    Main</div>;
 }
