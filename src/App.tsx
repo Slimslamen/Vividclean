b40887c858221835fa57284ea421d35b09483867
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-02-28 15:41:36 +0100

    Add KundSida component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from "./LandingFolder/Footer";
 import Landing from "./LandingFolder/Landing";
 import Navbar from "./LandingFolder/Navbar";
 import { ProductContextProvider } from "./ProductContext";
+import KundSida from "./Sidor/KundSida";
 
 
 function App() {
@@ -10,11 +11,10 @@ function App() {
 
   return (
     <ProductContextProvider>
-
         <Navbar />
         <Landing />
         <Footer />
-
+        <KundSida />
     </ProductContextProvider>
   );
 }
