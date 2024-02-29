3073499124d7a7593a7cb69e2cc90c7ae61dc353
martamalm <144331091+martamalm@users.noreply.github.com>
2024-02-29 14:23:59 +0100

    la till samma div som ändrar bakgrunden på register i landing

--- src/LandingFolder/Landing.tsx
+++ src/LandingFolder/Landing.tsx
@@ -6,13 +6,15 @@ import React from "react";
 import Contact from "./Contact";
 
 export default function Landing(): JSX.Element {
-  const { loginVisible } = React.useContext(ProductContext)! as ContextType;
+  const { loginVisible, registerVisible } = React.useContext(ProductContext)! as ContextType;
   return (
     <>
       <div className={` ${!loginVisible ? "" : "bg-black bg-opacity-80 z-10"}`}>
-       {/* <Contact/> */}
+      <div className={` ${!registerVisible ? "" : "bg-black bg-opacity-80 z-10"}`}>
+       <Contact/>
         <Header />
         <Main />
+      </div>
       </div>
     </>
   );
