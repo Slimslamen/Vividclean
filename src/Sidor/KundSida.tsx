9ab0c25bb0f4b6d706616bfbaf024e77dee4c52e
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-03-01 09:11:45 +0100

    Almost done with inputs

--- src/Sidor/KundSida.tsx
+++ src/Sidor/KundSida.tsx
@@ -13,8 +13,10 @@ interface Ibooking{
   id:string;
   name:string;
   date: Date
-  time: number;
-  service: string
+  cleaner: string;
+  time: string;
+  service: string;
+  status: boolean;
 }
 
 const options: Ioptions[] = [
@@ -48,13 +50,19 @@ const options: Ioptions[] = [
 export default function KundSida():JSX.Element {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [selectedTime, setSelectedTime] = useState<string>("")
-  const [booking, setBooking] = useState<Ibooking>({id:uuidv4(), name:"Sofia", date:selectedDate, time:selectedTime, service:"" })
+  const [selectedCleaner, setSelectedCleaner] = useState<string>("")
+  //Storing bookings
+  const [booking, setBooking] = useState<Ibooking>({id:uuidv4(), name:"Sofia", date:selectedDate, cleaner:"", time:selectedTime, service:selectedCleaner, status:false })
 
   
-
+//Functions that submits all the values when submitted. this function is dependable of the onchanges on the inputs.
   const handleSubmit = (e:FormEvent) => {
     e.preventDefault()
-    console.log(selectedTime);
+    console.log("time:",selectedTime);
+    console.log(`Date: ${selectedDate.getMonth()}/${selectedDate.getDate()}/${selectedDate.getFullYear()}`);
+    console.log("Service:", selectedCleaner);
+    
+    
   }
 
   return (
@@ -64,15 +72,16 @@ export default function KundSida():JSX.Element {
           <h1 className="text-5xl font-DM">{`${"Sofia"}s`} bokningar</h1>
           <div>
             <h2 className="text-3xl font-DM">Boka städning</h2>
-            <div className="flex flex-row justify-between py-5">
+            <div className="flex flex-row justify-between py-5 space-x-3 md:space-x-0">
               <DatePicker onChange={(date:Date) => setSelectedDate(date)} filterDate={date => { return date.getDay() !== 0 && date.getDay() !== 6;}}/* Disable weekends (Saturday and Sunday) */ minDate={new Date()} selected={selectedDate} />
               <input onChange={e => setSelectedTime(e.target.value)} value={selectedTime} id="time" type="time" min='08:00' max= '15:00' step="3600" className="p-1 rounded-lg w-5/12" />
             </div>
-            <Dropdown label="Städare" className="w-1/3" inline value={booking.service}>
-              <DropdownItem value="estelle">Estelle</DropdownItem>
-              <DropdownItem value="Marta">Märta</DropdownItem>
-              <DropdownItem value="Jimmy">Jimmy</DropdownItem>  
-            </Dropdown>
+            <select onChange={(e) => setSelectedCleaner(e.target.value)} name="Städare" id="Städare" className="p-1 rounded-lg w-4/12 bg-transparent focus:outline-none">
+              <option value="">Städare</option>
+              <option value="Estelle">Estelle</option>
+              <option value="Märta">Märta</option>
+              <option value="Jimmy">Jimmy</option>
+            </select>
             <ul className="mt-2 items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
               {options.map((option) => (
                 <li className="w-full px-2 hover:bg-customHoverDark duration-300 ease-in-out rounded-md">
