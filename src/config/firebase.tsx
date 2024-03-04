b75461f940589b5e0a6c3a081ef275a0cf0a6328
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-03-04 13:48:52 +0100

    firebase init

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";/* 
-import { getAnalytics } from "firebase/analytics";
- */// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDbJxahF_FwUHw1oVk4kjyvlQI_3lGvJic",
   authDomain: "vividclean-fc118.firebaseapp.com",
@@ -18,4 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-/* const analytics = getAnalytics(app); */
+const auth = getAuth(app)
+const db = getFirestore(app)
