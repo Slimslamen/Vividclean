572801ded0c62e0bc5c2da226b827f829c45b8fd
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-03-11 18:52:55 +0100

    loading

--- src/LandingFolder/Landing.tsx
+++ src/LandingFolder/Landing.tsx
@@ -4,10 +4,12 @@ import { ProductContext } from "../ProductContext";
 import { ContextType } from "../types/types";
 import React from "react";
 
+
 export default function Landing(): JSX.Element {
   const { loginVisible, registerVisible, adminVisible } = React.useContext(
     ProductContext
   )! as ContextType;
+
 
   return (
     <>
