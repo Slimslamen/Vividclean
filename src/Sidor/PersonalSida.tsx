813dc5344bef44f5212122acc402437a2a36bbd7
martamalm <144331091+martamalm@users.noreply.github.com>
2024-03-13 11:06:40 +0100

    småfix

--- src/Sidor/PersonalSida.tsx
+++ src/Sidor/PersonalSida.tsx
@@ -6,16 +6,13 @@ import UserAuthContext from "../UserAuthContext";
 
 export default function PersonalSida(): JSX.Element {
   const [cleaner, setCleaner] = useState<Ibooking[]>([]);
-  const [admin, setAdmin] =useState<string>("")
+  const [admin, setAdmin] = useState<string>("");
 
   const { emailAdmin } = React.useContext(
     UserAuthContext
   ) as UserAuthContextProps;
 
-  
-
   useEffect(() => {
-
     const fetchBookings = async () => {
       try {
         const firestore = getFirestore();
@@ -23,45 +20,34 @@ export default function PersonalSida(): JSX.Element {
           collection(firestore, "users", emailAdmin, "booking")
         );
         const getUsername = doc(firestore, "users", emailAdmin);
-  
-        const usernameSap = await getDoc(getUsername);
-        
 
-        const adminUser = usernameSap.data()?.username;
-       
-        setAdmin(adminUser)
-  
+        const usernameSnap = await getDoc(getUsername);
+        const adminUser = usernameSnap.data()?.username;
+
+        setAdmin(adminUser);
+
         const filteredBookings: Ibooking[] = bookingsRef.docs
           .filter((doc) => doc.data().cleaner === adminUser)
           .map((doc) => ({
             ...doc.data(),
             id: doc.id,
-            date: doc.data().date.toDate()
+            date: doc.data().date.toDate(),
           }));
-          setCleaner(filteredBookings)
-         
-      } 
-      
-      catch (error) {
+        setCleaner(filteredBookings);
+      } catch (error) {
         console.error("Error fetching bookings:", error);
       }
-  
     };
-   
 
     fetchBookings();
-
   }, []);
-
-
-
 
   return (
     <>
       <h1 className="font-DM text-5xl flex items-center justify-center mt-16 border-b border-grey">
         Personal: {admin}
       </h1>
-  
+
       <div className="flex md:flex-cols-2 md:gap-4">
         <div className="w-full h-96 mt-10 mb-10">
           <h2 className="font-DM text-xl flex items-center justify-center border-b border-black ml-4 mb-4">
@@ -71,7 +57,9 @@ export default function PersonalSida(): JSX.Element {
             {cleaner?.map((booking) => (
               <li className="m-5 border-b border-black" key={booking.id}>
                 Datum: {booking.date.toLocaleDateString()}, Tid: {booking.time}
-                <button className="m-5 ml-48 border border-solid text-white bg-customDark p-1 rounded-lg">städning utförd</button>
+                <button className="m-5 ml-48 border border-solid text-white bg-customDark p-1 rounded-lg">
+                  städning utförd
+                </button>
               </li>
             ))}
           </ul>
