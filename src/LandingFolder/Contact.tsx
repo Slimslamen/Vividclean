af8fc759c56fcc1c984458d6abad1fda703c93c9
martamalm <144331091+martamalm@users.noreply.github.com>
2024-04-01 13:03:36 +0200

    gjort om kontaktformuläret och gjort det responsivt

--- src/LandingFolder/Contact.tsx
+++ src/LandingFolder/Contact.tsx
@@ -2,46 +2,46 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="w-full h-auto flex items-center justify-center mt-10 mb-10 "> 
-    <h1 className="font-DM text-3xl">Vi finns här för att hjälpa dig, tveka inte på att kontaka oss! </h1>
-      <div className="w-80 h-auto border border-solid flex items-center justify-center bg-customBeige">
+    <div className="w-full h-auto mt-12 mb-12 bg-customBeige"> 
+    <h1 className="font-DM md:text-3xl text-2xl flex items-center justify-center mb-8 border-b border-gray-400 ml-28 mr-28 text-customDark">Kontakta VividClean</h1>
+      <div className="w-80 h-auto flex items-center justify-center md:ml-[39%] ml-[26%]">
         <form className="max-w-sm mx-auto">
           <div>
             <label
               htmlFor="small-input"
-              className="block mt-5 mb-5 text-sm font-DM font-bold text-gray-900 dark:text-white"
+              className="block mt-5 mb-5 text-m font-DM font-bold border-b border-gray-400 text-customDark dark:text-white"
             >
-              Namn:
+              Namn
             </label>
             <input
               type="text"
               id="small-input"
-              className="block w-full p-2 mb-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-2 mb-5 text-customDark text-xs dark:text-white"
             />
             <label
               htmlFor="small-input"
-              className="block mb-5 text-sm font-DM font-bold text-gray-900 dark:text-white"
+              className="block mb-5 text-m font-DM font-bold border-b border-gray-400 text-customDark dark:text-white"
             >
-              E-post:
+              E-post
             </label>
             <input
               type="text"
               id="small-input"
-              className="block w-full p-2 mb-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-2 mb-5 text-customDark text-xs dark:text-white"
             />
           </div>
           <label
             htmlFor="message"
-            className="block mb-5 text-sm font-DM font-bold text-gray-900 dark:text-white"
+            className="block mb-5 text-m font-DM font-bold border-b border-gray-400 text-customDark dark:text-white"
           >
-            Ditt meddelande:
+            Ditt meddelande
           </label>
           <textarea
             id="message"
-            className="p-2.5 mb-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="p-2.5 mb-5 w-full text-sm text-customDark dark:text-white"
             placeholder="Skriv ditt meddelande här"
           ></textarea>
-          <button type="submit" className="bg-customDark hover:bg-gray-700 rounded-lg text-white font-DM mb-10 p-2">
+          <button type="submit" className="bg-customDark hover:bg-gray-700 rounded-lg text-customBeige font-DM mb-10 p-2">
             Kontakta oss
           </button>
         </form>
